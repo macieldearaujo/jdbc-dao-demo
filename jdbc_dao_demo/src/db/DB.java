baf1b511bc6bd9912ad0458780751ddfc34128ca
douglasmcl <douglasmacielaraujo@outlook.com>
2024-03-14 16:04:45 -0300

    create the dbException

--- jdbc_dao_demo/src/db/DB.java
+++ jdbc_dao_demo/src/db/DB.java
@@ -1,5 +1,27 @@
 package db;
 
-public class DB {
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			
+		}
+	}
+	
+	private static Properties loadProperties() {
+		String path = "db.properties";
+		try(FileInputStream fs = new FileInputStream(path)) {
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
 }
