181895ef8d45226e57f859d53c4da7cc6dbf3770
douglasmcl <douglasmacielaraujo@outlook.com>
2024-03-18 16:40:45 -0300

    add the findByDepartment method in the dao interface

--- jdbc_dao_demo/src/application/Program.java
+++ jdbc_dao_demo/src/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import java.sql.Connection;
 import java.util.List;
 
-import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Seller;
 
 public class Program {
@@ -18,9 +15,8 @@ public class Program {
 		
 		Seller s2 = s1.findById(3);
 		//System.out.println(s2);
-		
-		SellerDaoJDBC s3 = new SellerDaoJDBC(DB.getConnection());
-		List<Seller> list = s3.findByDepartment(2);
+
+		List<Seller> list = s1.findByDepartment(2);
 		
 		for(Seller seller : list) {
 			System.out.println(seller);
